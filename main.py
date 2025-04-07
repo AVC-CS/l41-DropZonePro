3c9968c4b1f570bdd40d6d30d6c5f99a048cd308
DropZonePro <mkeav50@gmail.com>
2025-04-06 19:58:40 -0700

    Commit 1

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ def main():
 
     N = int(input('Enter the number N: '))
     result = []
+    i = 0
+    j = 1
 
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    while i <= N:
+        result.append(j)
+        j = j * 2
+        i += 1
+    print(result)
+
 
     ########################################
     # Do not delete the return statement
